61930066e1d378298657d7cb13f1a3f2c67c60ab
Harika Mahapatra <104854899+shrmp07@users.noreply.github.com>
2024-03-22 22:37:14 +0530

    Updated loops.java

--- loops.java
+++ loops.java
@@ -25,3 +25,45 @@ class Main {
 }
     }
 }
+
+/*pattern 2*/
+/*
+*
+**
+***
+****
+*****
+*/
+
+class Main {
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*pattern 3*/
+/*
+1
+12
+123
+1234
+12345
+*/
+
+class Main {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
+
+
